4c8427eb1390860c3b3764581acb75225381d02d
Joey Smith <joey.m.smith@outlook.com>
2025-06-18 21:44:46 -0700

    Initial Commit / Getting it to compile

---
+++ VariableInputBooleanOutcome/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.ML;
+using BinaryClassification;
+using BinaryClassification.logic;
+using BinaryClassification.models;
+
+MLContext mlContext = new MLContext(seed: 0);
+Prediction prediction = new Prediction();
+
+var model = prediction.Train(mlContext);
+prediction.Evaluate(mlContext, model);
+prediction.TestSinglePrediction(mlContext, model);
